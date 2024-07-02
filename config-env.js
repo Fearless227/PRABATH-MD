e8149e6345fe41e4c878b99b3796de8c3d7edb4c
Fearless227 <isaacedeh666@gmail.com>
2024-07-02 02:29:40 +0100

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-BOT_NUMBER: process.env.BOT_NUMBER,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
+const fs = require('fs');sudo apt update
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });sudo apt install git
+sudo apt install nodejs npm
+function convertToBool(text, fault = 'true') {git clone https://github.com/prabathLK/PRABATH-MD.git
+    return text === fault ? true : false;cd PRABATH-MD
+}npm install
+module.exports = {nano ~/.bash_profile
+SESSION_ID: process.env.SESSION_ID,PRABATH-MD~RXUVSZRT#zX-Ucw0zESkm_mNqklzuGXXBzOL9PpZSbE1GAH5BtmE
+BOT_NUMBER: process.env.BOT_NUMBER,2349132599907
+GITHUB_USERNAME: process.env.GITHUB_USERNAME,Fearless227
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,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
 };
